b3b7be630bae5d178dae26a8d1392d3f6d61cd23
Kasun Kavinda <kkhettiarachchi10@gmail.com>
2024-11-18 10:12:06 +0530

    implement signup flow

---
+++ controller/auth.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require('../model/user')
+
+
+router.post("/signUp", async (req, res) => {
+
+    let user = await User.findOne({
+        email: req.body.email
+    });
+
+    if (user) return res.status(200).send("User already registered.");
+
+    user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        image: req.body.image
+    });
+
+
+    await user.save();
+
+    console.log(user);
+    res.status(200).send(user);
+
+});
+
+module.exports = router;
